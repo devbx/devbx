bd0ec8a4bc275edbb7e6d0fed9c6e75a9e3a65d3
Pang YS <161192932+devbx@users.noreply.github.com>
2024-07-05 22:47:25 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf .repo/local_manifests/
+repo init -u https://github.com/Evolution-XYZ/manifest.git -b udc --git-lfs
+git clone https://github.com/devbx/local_manifests --depth 1 -b evo-u .repo/local_manifests
+export BUILD_USERNAME=devbx
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export TZ=Asia/Kuching
+source build/envsetup.sh
+lunch lineage_earth-userdebug
+m evolution
