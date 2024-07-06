ea046373011a028281c520e0ed33a8f2770b0aa1
Pang YS <161192932+devbx@users.noreply.github.com>
2024-07-07 00:21:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 rm -rf .repo/local_manifests/
 repo init -u https://github.com/Evolution-XYZ/manifest.git -b udc --git-lfs
 git clone https://github.com/devbx/local_manifests --depth 1 -b evo-u .repo/local_manifests
+/opt/crave/resync.sh
 export BUILD_USERNAME=devbx
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
