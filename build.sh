eed32cb8a52e8d7c5bea705f19e858e70e9ba477
Pang YS <161192932+devbx@users.noreply.github.com>
2024-07-08 21:30:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 rm -rf .repo/local_manifests/
-repo init -u https://github.com/Evolution-XYZ/manifest.git -b udc --git-lfs
-git clone https://github.com/devbx/local_manifests.git -b evo-u .repo/local_manifests
+repo init -u https://github.com/DroidX-UI/manifest.git -b 14_v3 --git-lfs
+git clone https://github.com/devbx/local_manifests.git -b dxui .repo/local_manifests
 /opt/crave/resync.sh
 export BUILD_USERNAME=devbx
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export KBUILD_USERNAME=devbx
+export KBUILD_HOSTNAME=crave
 export TZ=Asia/Kuching
 source build/envsetup.sh
-lunch lineage_earth-userdebug
-m evolution
+lunch droidx_earth-ap2a-userdebug
+make installclean
+m bacon
