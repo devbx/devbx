91cd08d28ec5f463693a1a0879857f7022400567
Pang YS <161192932+devbx@users.noreply.github.com>
2024-07-11 14:57:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf .repo/local_manifests/
-repo init -u https://github.com/DroidX-UI/manifest.git -b 14_v3 --git-lfs
-git clone https://github.com/devbx/local_manifests.git -b dxui .repo/local_manifests
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+git clone https://github.com/devbx/local_manifests.git -b matrixx .repo/local_manifests
 /opt/crave/resync.sh
 export BUILD_USERNAME=devbx
 export BUILD_HOSTNAME=crave
@@ -10,6 +10,5 @@ export KBUILD_USERNAME=devbx
 export KBUILD_HOSTNAME=crave
 export TZ=Asia/Kuching
 source build/envsetup.sh
-lunch droidx_earth-ap2a-userdebug
-make installclean
-m bacon
+breakfast earth
+brunch earth
