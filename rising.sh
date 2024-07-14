60a9cfcd4c0216a1452c4340a15ea97ad6ef37cd
Pang YS <161192932+devbx@users.noreply.github.com>
2024-07-14 18:42:37 +0800

    Create rising.sh

---
+++ rising.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -rf .repo/local_manifests/
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+git clone https://github.com/devbx/local_manifests.git -b rising .repo/local_manifests
+/opt/crave/resync.sh
+export BUILD_USERNAME=devbx
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export KBUILD_USERNAME=devbx
+export KBUILD_HOSTNAME=crave
+export TZ=Asia/Kuching
+source build/envsetup.sh
+riseup earth userdebug
+rise b
+rise fb
