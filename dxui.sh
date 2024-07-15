bc4764c7a5bd4e202ed202f52b94a8e3f1c5e2f7
Pang YS <161192932+devbx@users.noreply.github.com>
2024-07-15 15:50:31 +0800

    Create dxui.sh

---
+++ dxui.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf .repo/local_manifests/
+repo init --depth=1 -u https://github.com/DroidX-UI/manifest.git -b 14_v3 --git-lfs
+git clone https://github.com/devbx/local_manifests.git -b dxui .repo/local_manifests
+/opt/crave/resync.sh
+export BUILD_USERNAME=devbx
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export KBUILD_USERNAME=devbx
+export KBUILD_HOSTNAME=crave
+export TZ=Asia/Kuching
+source build/envsetup.sh
+lunch droidx_earth-ap2a-userdebug
+m bacon
