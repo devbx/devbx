004409eca5f3d2ac083c3c3990575b40928f2ce4
Pang YS <161192932+devbx@users.noreply.github.com>
2024-08-06 22:38:26 +0800

    Create everest.sh

---
+++ everest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -rf .repo/local_manifests/
+repo init -u https://github.com/ProjectEverest/manifest -b qpr3 --git-lfs
+git clone https://github.com/devbx/local_manifests.git -b everest .repo/local_manifests
+/opt/crave/resync.sh
+export BUILD_USERNAME=devbx
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export KBUILD_USERNAME=devbx
+export KBUILD_HOSTNAME=crave
+export TZ=Asia/Kuching
+source build/envsetup.sh
+peak earth userdebug
+make installclean
+climb m
+climb mb
